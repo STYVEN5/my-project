b968d31d5e1b5f654717cd081ffc0720d9bfd2e9
STYVEN5 <garrymod291@gmail.com>
2026-03-18 22:47:44 +0500

    Redirect root to sites index for auth and include all previous breadcrumbs changes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('sites.index');
 });
 
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
